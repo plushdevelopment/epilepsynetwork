c443a5fa8e57d9a5adcdee966e8e77182f26e834
Ross Chapman <ross_chapman@me.com>
2015-04-25 14:03:24 -0400

    Added a few view controllers

---
+++ EpilepsyUnited/EpilepsyUnited/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  EpilepsyUnited
+//
+//  Created by Ross Chapman on 4/25/15.
+//  Copyright (c) 2015 Pratikbhai Patel. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+}
---
+++ EpilepsyUnited/EpilepsyUnited/NewsFeedViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewsFeedViewController.swift
+//  EpilepsyUnited
+//
+//  Created by Ross Chapman on 4/25/15.
+//  Copyright (c) 2015 Pratikbhai Patel. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+    
+}
