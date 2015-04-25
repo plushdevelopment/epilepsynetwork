37f007faae7552c1e53421e7cfeabb4a0f8c8dab
Pratikbhai Patel <pratikpatel@me.com>
2015-04-25 10:11:26 -0400

    Set up Xcode project

---
+++ EpilepsyUnited/EpilepsyUnited/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EpilepsyUnited
+//
+//  Created by Pratikbhai Patel on 4/25/15.
+//  Copyright (c) 2015 Pratikbhai Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
